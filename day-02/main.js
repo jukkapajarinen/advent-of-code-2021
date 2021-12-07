4fda8672ebe89de70ef03e1ac7b49fd4e2ad1654
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2021-12-08 00:10:02 +0200

    Solution for AoC day 2 second puzzle.

--- day-02/main.js
+++ day-02/main.js
@@ -15,5 +15,18 @@ rows.forEach(row => {
   }
 });
 
+let [horizontal2, depth2, aim] = [0, 0, 0]
+rows.forEach(row => {
+  const cmd = row.split(" ")
+  switch (cmd[0]) {
+    case "forward":
+      horizontal2 += Number(cmd[1]);
+      depth2 += aim * Number(cmd[1]);
+      break;
+    case "up": aim -= Number(cmd[1]); break;
+    case "down": aim += Number(cmd[1]); break;
+  }
+});
+
 console.log(`horizontal * depth: ${horizontal * depth} (part 1)`);
-console.log(`Another something: ${2} (part 2)`);
+console.log(`horizontal2 * depth2: ${horizontal2 * depth2} (part 2)`);
